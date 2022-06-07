23508f5a2ff833e15b799a95e8b13ba831440ca3
asty220 <85670776+asty220@users.noreply.github.com>
2022-06-07 15:23:49 +0900

    Revert "三村 色々見やすくして秒数を分かりやすく変えられるようにしました"

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import sys
 
 
 class Screen:
-    def __init__(self, fn, wh, title):
+    def __init__(self,fn,wh,title):
         #fn:背景画像,wh:高さ、横幅,title:
         pg.display.set_caption(title)
         self.width, self.height=wh   #(1600, 900)
@@ -13,8 +13,10 @@ class Screen:
         self.image = pg.image.load(fn)
 
 class cursor(pg.sprite.Sprite):
+
     def __init__(self, fn, r):
         super().__init__()
+        pg.sprite.Sprite.__init__(self,self.containers)
         self.image = pg.image.load(fn)   #sarface
         self.image = pg.transform.rotozoom(self.image, 0, r)
         self.rect = self.image.get_rect()   #rect
@@ -23,9 +25,12 @@ class cursor(pg.sprite.Sprite):
     def update(self):
         self.rect.center=pg.mouse.get_pos()
 
+
+
 class Bomb(pg.sprite.Sprite):
     def __init__(self, fn, r, screen):
         super().__init__()
+        pg.sprite.Sprite.__init__(self,self.containers)
         self.image = pg.image.load(fn)   #sarface
         self.image = pg.transform.rotozoom(self.image, 0, r)
         self.rect = self.image.get_rect()   #rect
@@ -40,27 +45,15 @@ class Music:
         pg.mixer.music.play()
 
 def main():
-    sc = 0
-    time = 0
-    
-    piclist = ["sozai/0.png","sozai/1.png","sozai/2.png","sozai/3.png","sozai/4.png","sozai/5.png","sozai/6.png","sozai/7.png","sozai/8.png","sozai/9.png","sozai/ぱっちぃ.png"]
+    global sc,time,musiclist,piclist
 
-    second = 30 #秒表記で管理(三村)
-    timeset = second * 144
-    
-    cursor.containers = pg.sprite.RenderUpdates()
-    Bomb.containers = pg.sprite.Group()
-    clock = pg.time.Clock()
+    timeset=4320
+    cursor.containers=pg.sprite.RenderUpdates()
+    Bomb.containers=pg.sprite.Group()
+    clock=pg.time.Clock()
 
-    screen = Screen("sozai/pg_bg.jpg", (1600,900), "エイム練習")             #スクリーンの生成
+    screen=Screen("sozai/pg_bg.jpg",(1600,900),"エイム練習")             #スクリーンの生成
     screen.disp.blit(screen.image, (0,0))
-
-    cursors = pg.sprite.Group()                       #照準の描写
-    cursors.add(cursor("sozai/nc151920.png", 0.1))
-
-    target = pg.sprite.Group()                #的の描写
-    for _ in range(2):
-        target.add(Bomb(piclist[random.randint(0,len(piclist)-1)], 1, screen))
 
     cursors=pg.sprite.Group()                       #照準の描写
     cursors.add(cursor("sozai/nc151920.png",0.1))
@@ -68,6 +61,7 @@ def main():
     target=pg.sprite.Group()                #的の描写
     for _ in range(2):
         target.add(Bomb(piclist[random.randint(0,len(piclist)-1)],1,screen))
+    
 
     while True:
         screen.disp.blit(screen.image, (0,0))
@@ -85,51 +79,49 @@ def main():
         cursors.draw(screen.disp)
 
 
-        if event.type == pg.MOUSEBUTTONDOWN:          #マウスをクリックした際に読み込まれる
+        if event.type==pg.MOUSEBUTTONDOWN:          #マウスをクリックした際に読み込まれる
             Music("sozai/nc72338.wav")         #音を再生する
-            if len(pg.sprite.groupcollide(cursors, target, 0, 1))!=0:             #的に重なっているときに読み込まれる
+            if len(pg.sprite.groupcollide(cursors,target,0,1))!=0:             #的に重なっているときに読み込まれる
                     sc+=1                                                       #カーソルと重なっていた的を削除し新しい的を生成する
-                    target.add(Bomb(piclist[random.randint(0,len(piclist)-1)], 1, screen))
+                    target.add(Bomb(piclist[random.randint(0,len(piclist)-1)],1,screen))
                     if len(target)<=1:                                          #万が一的が同時に消えてしまった場合に動き的を補充する
-                        target.add(Bomb(piclist[random.randint(0,len(piclist)-1)], 1, screen))
+                        target.add(Bomb(piclist[random.randint(0,len(piclist)-1)],1,screen))
         
         font=pg.font.Font(None,30)
-
-        score=font.render(f"score:{str(sc)}", True, "BLACK")
-        screen.disp.blit(score, (10,10))
-
-        timelimit=font.render(f"timelimit:{second-time//144}", True, "BLACK")
-        screen.disp.blit(timelimit, (10,30))
-
+        score=font.render(f"score:{str(sc)}",True,"BLACK")
+        screen.disp.blit(score,(10,10))
+        timelimit=font.render(f"timelimit:{timeset//144-time//144}",True,"BLACK")
+        screen.disp.blit(timelimit,(10,30))
         pg.display.update()  # 画面の更新
         clock.tick(144)
-        time += 1
+        time+=1
 
-        if time >= timeset:
-            scores(sc)
+        if time>=timeset:
+            scores()
 
-def scores(sc):      #ゲームオーバー画面を表示するための関数
-    
-    musiclist = ["sozai/nc211934.wav", "sozai/nc133067.wav", "sozai/nc127260.mp3", "sozai/nc67013.wav", "sozai/nc197899.wav"]
-    txtlist = ["GAME OVER", f"Your Score:{sc}!", "Exit:Press 'ESCAPE' key", "Restart:Press 'R' Key"]
+def scores():      #ゲームオーバー画面を表示するための関数
+    global time,sc,musiclist
+    txtlist=[f"GAME OVER",f"Your Score:{sc}!",f"Exit:Press 'ESCAPE' key",f"Restart:Press 'R' Key"]
     while True:
         pg.display.set_caption("game over")         
-        bsc = pg.display.set_mode((1600,900))
+        bsc=pg.display.set_mode((1600,900))
         bsc_rect=bsc.get_rect()
         bsc.blit(bsc,bsc_rect)
-        
         font=pg.font.Font(None,60)
-        for i in range(len(txtlist)):              #文字の表示
-            bsc.blit(font.render(txtlist[i], True, "WHITE"), (400,300+60*i))
 
-        key_list = pg.key.get_pressed()           #キー入力に対応した動作
+        for i in range(len(txtlist)):              #文字の表示
+            bsc.blit(font.render(txtlist[i],True,"WHITE"),(400,300+60*i))
+
+        key_list=pg.key.get_pressed()           #キー入力に対応した動作
         if key_list[pg.K_ESCAPE]:
             pg.quit()
             sys.exit()
         if key_list[pg.K_r]:
-            Music(musiclist[random.randint(0, len(musiclist)-1)])
+            if time>=1:
+                time=0
+                sc=0
+            Music(musiclist[random.randint(0,len(musiclist)-1)])
             main()
-
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
@@ -137,25 +129,23 @@ def scores(sc):      #ゲームオーバー画面を表示するための関数
         pg.display.update()         # 画面の更新
 
 def start():            #スタート画面の表示
-    timecount = 0
+    timecount=0
     while True:
         pg.display.set_caption("Start")
-        bs = pg.display.set_mode((1600,900))
-        bsc_rect = bs.get_rect()
+        bs=pg.display.set_mode((1600,900))
+        bsc_rect=bs.get_rect()
         bs.blit(bs,bsc_rect)
-        font = pg.font.Font(None,80)
-        txt2 ="START!"
+        font=pg.font.Font(None,80)
+        font1=pg.font.Font(None,60)
+        txt2=f"START!"
+        txt4=f"Press 'S' key!"
         score3=font.render(txt2,True,"WHITE")
         bs.blit(score3,(400,350))
-
-        font1=pg.font.Font(None,60)
-        txt4 = "Press 'S' key!"
         score4=font1.render(txt4,True,"WHITE")
         bs.blit(score4,(400,400))
-
         key_list=pg.key.get_pressed()
-        timecount += 1
-        if timecount == 144:
+        timecount+=1
+        if timecount==144:
             Music("sozai/nc133938.wav")    #スタート時の音声
         if key_list[pg.K_s]:
             main()              #main関数を実行
@@ -163,8 +153,12 @@ def start():            #スタート画面の表示
 
 
 if __name__ == "__main__":
-    pg.init()
-    s = pg.mixer.Sound("sozai/フリージア.mp3")
+    sc=0
+    time=0
+    musiclist=["sozai/nc211934.wav","sozai/nc133067.wav","sozai/nc127260.mp3","sozai/nc67013.wav","sozai/nc197899.wav"]
+    piclist=["sozai/0.png","sozai/1.png","sozai/2.png","sozai/3.png","sozai/4.png","sozai/5.png","sozai/6.png","sozai/7.png","sozai/8.png","sozai/9.png","sozai/ぱっちぃ.png"]
+    pg.init() 
+    s=pg.mixer.Sound("sozai/フリージア.mp3")
     s.set_volume(0.5)
     s.play(loops=-1)
     start()
